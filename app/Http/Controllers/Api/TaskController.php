67df48ab6d27e16a4e2467767d83cf0f43988200
Davidvl <josuedavidvl18@gmail.com>
2023-08-05 00:09:36 -0600

    CRUD actualizado

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Task;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Task::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+
+        $task = Task::create([
+            'title' => $request->input('title'),
+            'completed' => false
+        ]);
+
+        return $task;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return $task;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+
+        return $task;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json(null, 204);
+    }
+}
